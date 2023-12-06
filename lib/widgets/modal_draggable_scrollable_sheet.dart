0f244d6a0c04fefdca326cae6c71b9dc3cb927f2
BlastTea <arya.jbr.999@gmail.com>
2023-12-06 21:24:59 +0700

    m_widget v2.3.1

--- lib/widgets/modal_draggable_scrollable_sheet.dart
+++ lib/widgets/modal_draggable_scrollable_sheet.dart
@@ -3,12 +3,30 @@ part of 'widgets.dart';
 class ModalDraggableScrollableSheet extends StatelessWidget {
   const ModalDraggableScrollableSheet({
     super.key,
+    this.controller,
     required this.builder,
     this.withDragHandle = true,
+    this.expand = true,
+    this.initialChildSize = 0.5,
+    this.maxChildSize = 1.0,
+    this.minChildSize = 0.25,
+    this.shouldCloseOnMinExtent = true,
+    this.snap = false,
+    this.snapAnimationDuration,
+    this.snapSizes,
   });
 
+  final DraggableScrollableController? controller;
   final Widget Function(BuildContext context, ScrollController scrollController) builder;
   final bool withDragHandle;
+  final bool expand;
+  final double initialChildSize;
+  final double maxChildSize;
+  final double minChildSize;
+  final bool shouldCloseOnMinExtent;
+  final bool snap;
+  final Duration? snapAnimationDuration;
+  final List<double>? snapSizes;
 
   static show({
     required Widget Function(BuildContext context) builder,
@@ -53,6 +71,15 @@ class ModalDraggableScrollableSheet extends StatelessWidget {
         child: Container(
           color: const Color.fromRGBO(0, 0, 0, 0.001),
           child: DraggableScrollableSheet(
+            controller: controller,
+            expand: expand,
+            initialChildSize: initialChildSize,
+            maxChildSize: maxChildSize,
+            minChildSize: minChildSize,
+            shouldCloseOnMinExtent: shouldCloseOnMinExtent,
+            snap: snap,
+            snapAnimationDuration: snapAnimationDuration,
+            snapSizes: snapSizes,
             builder: (context, scrollController) => Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).colorScheme.surface,
