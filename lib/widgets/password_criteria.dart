076684e30a215557160256ca5b2edf5648dbd819
BlastTea <arya.jbr.999@gmail.com>
2025-02-01 21:41:09 +0700

    change: password criteria private class to public

--- lib/widgets/password_criteria.dart
+++ lib/widgets/password_criteria.dart
@@ -18,15 +18,15 @@ class PasswordCriteriaInfo extends StatelessWidget {
 
     for (PasswordCriteria criteria in criterias) {
       switch (criteria) {
-        case _PasswordCriteriaMinimunChacater value:
+        case PasswordCriteriaMinimunChacater value:
           if (text.length >= value.minLength) results.add(value);
-        case _PasswordCriteriaContainsLowerCaseLetters value:
+        case PasswordCriteriaContainsLowerCaseLetters value:
           if (text.contains(RegExp(r'[a-z]'))) results.add(value);
-        case _PasswordCriteriaContainsUpperCaseLetters value:
+        case PasswordCriteriaContainsUpperCaseLetters value:
           if (text.contains(RegExp(r'[A-Z]'))) results.add(value);
-        case _PasswordCriteriaContainsNumber value:
+        case PasswordCriteriaContainsNumber value:
           if (text.contains(RegExp(r'[0-9]'))) results.add(value);
-        case _PasswordCriteriaContainsSpecialCharacters value:
+        case PasswordCriteriaContainsSpecialCharacters value:
           if (text.contains(RegExp(r'[!@#\$%^&*()_+{}[\]:;<>,.?~\\-]'))) results.add(value);
       }
     }
@@ -54,15 +54,15 @@ class PasswordCriteriaInfo extends StatelessWidget {
 sealed class PasswordCriteria {
   PasswordCriteria();
 
-  factory PasswordCriteria.minimumCharacters({required int minLength}) => _PasswordCriteriaMinimunChacater(minLength: minLength);
+  factory PasswordCriteria.minimumCharacters({required int minLength}) => PasswordCriteriaMinimunChacater(minLength: minLength);
 
-  factory PasswordCriteria.containsLowerCaseLetters() = _PasswordCriteriaContainsLowerCaseLetters;
+  factory PasswordCriteria.containsLowerCaseLetters() = PasswordCriteriaContainsLowerCaseLetters;
 
-  factory PasswordCriteria.containsUpperCaseLetters() = _PasswordCriteriaContainsUpperCaseLetters;
+  factory PasswordCriteria.containsUpperCaseLetters() = PasswordCriteriaContainsUpperCaseLetters;
 
-  factory PasswordCriteria.containsNumber() = _PasswordCriteriaContainsNumber;
+  factory PasswordCriteria.containsNumber() = PasswordCriteriaContainsNumber;
 
-  factory PasswordCriteria.containsSpecialCharacters() = _PasswordCriteriaContainsSpecialCharacters;
+  factory PasswordCriteria.containsSpecialCharacters() = PasswordCriteriaContainsSpecialCharacters;
 
   static List<PasswordCriteria> getAllvalues({required int minLength}) => [
         PasswordCriteria.minimumCharacters(minLength: minLength),
@@ -75,8 +75,8 @@ sealed class PasswordCriteria {
   String get text;
 }
 
-class _PasswordCriteriaMinimunChacater extends PasswordCriteria {
-  _PasswordCriteriaMinimunChacater({required this.minLength});
+class PasswordCriteriaMinimunChacater extends PasswordCriteria {
+  PasswordCriteriaMinimunChacater({required this.minLength});
 
   final int minLength;
 
@@ -84,22 +84,22 @@ class _PasswordCriteriaMinimunChacater extends PasswordCriteria {
   String get text => Language.getInstance().getValue('Minimum {0} characters', [minLength])!;
 }
 
-class _PasswordCriteriaContainsLowerCaseLetters extends PasswordCriteria {
+class PasswordCriteriaContainsLowerCaseLetters extends PasswordCriteria {
   @override
   String get text => Language.getInstance().getValue('Contains lowercase letters')!;
 }
 
-class _PasswordCriteriaContainsUpperCaseLetters extends PasswordCriteria {
+class PasswordCriteriaContainsUpperCaseLetters extends PasswordCriteria {
   @override
   String get text => Language.getInstance().getValue('Contains uppercase letters')!;
 }
 
-class _PasswordCriteriaContainsNumber extends PasswordCriteria {
+class PasswordCriteriaContainsNumber extends PasswordCriteria {
   @override
   String get text => Language.getInstance().getValue('Contains numbers')!;
 }
 
-class _PasswordCriteriaContainsSpecialCharacters extends PasswordCriteria {
+class PasswordCriteriaContainsSpecialCharacters extends PasswordCriteria {
   @override
   String get text => Language.getInstance().getValue('Contains special characters')!;
 }
