751fcf6a140dc52f908c7b54b3a0c57b3fbb0d7c
BlastTea <arya.jbr.999@gmail.com>
2026-06-06 03:59:35 +0700

    feat(input): added separator format controller

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -12,6 +12,7 @@ part 'material_utils.dart';
 part 'firestore_auto_id_generator.dart';
 part 'custom_scroll_behavior.dart';
 part 'adaptive_dialog_route.dart';
+part 'text_editing_controller_separator_format.dart';
 part 'text_editing_controller_thousand_format.dart';
 part 'submit_focus_node.dart';
 part 'dialog_utils.dart';
@@ -35,31 +36,49 @@ const double kBottomFabPadding = 84.0;
 const double kMinimumDraggableScrollableHeight = 94.0;
 const double kMaximumDescriptionHeight = 400.0;
 
-const String shortLorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis ipsum tortor, cursus volutpat sapien iaculis ac. Vivamus eu dui pharetra, accumsan eros non, placerat sem.';
-const String longLorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis ipsum tortor, cursus volutpat sapien iaculis ac. Vivamus eu dui pharetra, accumsan eros non, placerat sem. Etiam pulvinar venenatis ligula, non condimentum lorem blandit nec. Nam elementum in tellus id viverra. Aliquam erat volutpat. Suspendisse placerat efficitur diam at laoreet. Maecenas feugiat purus sem, in malesuada sapien aliquam quis. Vestibulum ut velit eget massa egestas viverra non et turpis. Morbi fermentum pellentesque molestie. Fusce tempus neque nec justo iaculis aliquet. Duis consequat consequat aliquet.';
+const String shortLorem =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis ipsum tortor, cursus volutpat sapien iaculis ac. Vivamus eu dui pharetra, accumsan eros non, placerat sem.';
+const String longLorem =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis ipsum tortor, cursus volutpat sapien iaculis ac. Vivamus eu dui pharetra, accumsan eros non, placerat sem. Etiam pulvinar venenatis ligula, non condimentum lorem blandit nec. Nam elementum in tellus id viverra. Aliquam erat volutpat. Suspendisse placerat efficitur diam at laoreet. Maecenas feugiat purus sem, in malesuada sapien aliquam quis. Vestibulum ut velit eget massa egestas viverra non et turpis. Morbi fermentum pellentesque molestie. Fusce tempus neque nec justo iaculis aliquet. Duis consequat consequat aliquet.';
 
-TextInputFormatter textFormatterDoubleWithNegative = FilteringTextInputFormatter.allow(RegExp(r'^-?\d+\.?\d*$'));
-TextInputFormatter textFormatterDoubleOnly = FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d*$'));
-TextInputFormatter textFormattedDigitsWithNegative = FilteringTextInputFormatter.allow(RegExp(r'^-?\d+'));
-TextInputFormatter textFormatterDigitsOnly = FilteringTextInputFormatter.digitsOnly;
-TextInputFormatter textFormatterSingleLine = FilteringTextInputFormatter.singleLineFormatter;
-TextInputFormatter textFormatterLetterOnly = FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z]'));
-TextInputFormatter textFormatterLetterDigitsOnly = FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]'));
+TextInputFormatter textFormatterDoubleWithNegative =
+    FilteringTextInputFormatter.allow(RegExp(r'^-?\d+\.?\d*$'));
+TextInputFormatter textFormatterDoubleOnly = FilteringTextInputFormatter.allow(
+  RegExp(r'^\d+\.?\d*$'),
+);
+TextInputFormatter textFormattedDigitsWithNegative =
+    FilteringTextInputFormatter.allow(RegExp(r'^-?\d+'));
+TextInputFormatter textFormatterDigitsOnly =
+    FilteringTextInputFormatter.digitsOnly;
+TextInputFormatter textFormatterSingleLine =
+    FilteringTextInputFormatter.singleLineFormatter;
+TextInputFormatter textFormatterLetterOnly = FilteringTextInputFormatter.allow(
+  RegExp(r'[a-zA-Z]'),
+);
+TextInputFormatter textFormatterLetterDigitsOnly =
+    FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]'));
 
 /// Generates responsive horizontal padding based on the screen size.
 ///
 /// The [size] parameter is the current screen size.
 ///
 /// Returns the appropriate [EdgeInsets] for horizontal padding.
-EdgeInsets responsivePadding(Size size, {double minimumHorizontalPadding = 16.0}) {
+EdgeInsets responsivePadding(
+  Size size, {
+  double minimumHorizontalPadding = 16.0,
+}) {
   if (size.width < kCompactSize) {
     return EdgeInsets.symmetric(horizontal: minimumHorizontalPadding);
   }
   if (size.width <= kMediumSize) {
-    return EdgeInsets.symmetric(horizontal: (size.width - kCompactSize) / 2 + minimumHorizontalPadding);
+    return EdgeInsets.symmetric(
+      horizontal: (size.width - kCompactSize) / 2 + minimumHorizontalPadding,
+    );
   }
 
-  return EdgeInsets.symmetric(horizontal: (size.width - kMediumSize) / 2 + minimumHorizontalPadding);
+  return EdgeInsets.symmetric(
+    horizontal: (size.width - kMediumSize) / 2 + minimumHorizontalPadding,
+  );
 }
 
 /// Calculates responsive width based on the screen size.
@@ -83,7 +102,10 @@ double responsiveWidth(Size size, {double minimumHorizontalPadding = 16.0}) {
 /// The [size] parameter is the current screen size.
 ///
 /// Returns the appropriate width for responsive dialogs.
-double responsiveDialogWidth(Size size, {double minimumHorizontalPadding = 16.0}) {
+double responsiveDialogWidth(
+  Size size, {
+  double minimumHorizontalPadding = 16.0,
+}) {
   if (size.width < kCompactSize) {
     return size.width - minimumHorizontalPadding * 2.0;
   }
@@ -97,48 +119,65 @@ double responsiveDialogWidth(Size size, {double minimumHorizontalPadding = 16.0}
 /// The [animation] parameter is an optional animation value for color transition.
 ///
 /// Returns the selected tile color for the specified theme mode.
-Color selectedTileColor({Animation<double>? animation}) => animation != null ? Color.lerp(Colors.transparent, Theme.of(navigatorKey.currentContext!).colorScheme.secondaryContainer, animation.value)! : Theme.of(navigatorKey.currentContext!).colorScheme.secondaryContainer;
+Color selectedTileColor({Animation<double>? animation}) => animation != null
+    ? Color.lerp(
+        Colors.transparent,
+        Theme.of(navigatorKey.currentContext!).colorScheme.secondaryContainer,
+        animation.value,
+      )!
+    : Theme.of(navigatorKey.currentContext!).colorScheme.secondaryContainer;
 
 /// Animates the given [scrollController] to a specified [offset].
 ///
 /// The [offset] parameter specifies the desired offset to scroll to.
 ///
 /// The [scrollController] parameter is the ScrollController to be animated.
-Future<void> animateScrollController({required double offset, required ScrollController scrollController}) => scrollController.animateTo(
-      offset,
-      duration: Durations.medium2,
-      curve: Curves.fastOutSlowIn,
-    );
+Future<void> animateScrollController({
+  required double offset,
+  required ScrollController scrollController,
+}) => scrollController.animateTo(
+  offset,
+  duration: Durations.medium2,
+  curve: Curves.fastOutSlowIn,
+);
 
 /// Animates the given [controller] of a DraggableScrollableSheet to a specified [size].
 ///
 /// The [size] parameter specifies the desired size to animate the sheet to.
 ///
 /// The [controller] parameter is the DraggableScrollableController to be animated.
-Future<void> animateDraggableScrollableController({required double size, required DraggableScrollableController controller}) => controller.animateTo(
-      size,
-      duration: Durations.short2,
-      curve: Curves.fastOutSlowIn,
-    );
+Future<void> animateDraggableScrollableController({
+  required double size,
+  required DraggableScrollableController controller,
+}) => controller.animateTo(
+  size,
+  duration: Durations.short2,
+  curve: Curves.fastOutSlowIn,
+);
 
 /// Animates the given [pageController] to a specified [page].
 ///
 /// The [page] parameter specifies the desired page to scroll to.
 ///
 /// The [pageController] parameter is the PageController to be animated.
-Future<void> animatePageController({required int page, required PageController pageController}) => pageController.animateToPage(
-      page,
-      duration: Durations.short2,
-      curve: Curves.fastOutSlowIn,
-    );
+Future<void> animatePageController({
+  required int page,
+  required PageController pageController,
+}) => pageController.animateToPage(
+  page,
+  duration: Durations.short2,
+  curve: Curves.fastOutSlowIn,
+);
 
 String jsonToCorrectStringFormat(String jsonString) {
   String words = jsonString;
 
-  while (words.characters.elementAt(0) == '\'' || words.characters.elementAt(0) == '"') {
+  while (words.characters.elementAt(0) == '\'' ||
+      words.characters.elementAt(0) == '"') {
     words = words.substring(1);
   }
-  while (words.characters.elementAt(words.length - 1) == '\'' || words.characters.elementAt(words.length - 1) == '"') {
+  while (words.characters.elementAt(words.length - 1) == '\'' ||
+      words.characters.elementAt(words.length - 1) == '"') {
     words = words.substring(0, words.length - 1);
   }
 
